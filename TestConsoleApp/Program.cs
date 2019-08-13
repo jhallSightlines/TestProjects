93ba3fadc35259821345277d9bf19dcb95ef453b
GORDIAN-GROUP\jhall <jhall@sightlinesllc.com>
2019-08-13 09:13:32 -0400

    Second change for today.

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -10,8 +10,10 @@ namespace TestConsoleApp
     {
         static void Main(string[] args)
         {
+			Console.WriteLine("Second new line on 8/13.");
 			Console.WriteLine("Edit from second user.");
-			Console.WriteLine("NewLine for 06252019");            Console.WriteLine("Hello World from Jim.");
+			Console.WriteLine("NewLine for 06252019");            
+			Console.WriteLine("Hello World from Jim.");
             Console.WriteLine("Another branch made an edit.");
             Console.WriteLine("New line.");
             Console.WriteLine("New line on 8/13.");
